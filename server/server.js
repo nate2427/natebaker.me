74106734cec978028be6b9353806b1cc127ed9a0
Street Coder Nate <natebaker2427@gmail.com>
2021-07-16 11:22:05 -0400

    updated the server to be able to refresh.

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ const PORT = process.env.PORT || 54321;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../build")));
 app.use(apiRoutes);
+
 app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build/index.html"));
+});
+
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../build/index.html"));
 });
 
